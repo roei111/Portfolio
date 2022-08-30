d4457e643881ec4a280c53cd3d8a56ad34e26e72
RoeiY <roeiyaacobi@gmail.com>
2022-08-30 23:29:42 +0300

    Create work experience section

---
+++ src/data/experienceData.js
@@ -0,0 +1,15 @@
+const jobs = [
+  {
+    title: "Full-Stack Web Developer",
+    company: "Brand Market",
+    range: "January - August 2022",
+    descriptions: [
+      "Worked in a team of web developers on developing and implementing web applications using <b>JavaScript</b>, <b>React.js</b>, and <b>Ruby on Rails</b>",
+      "Writing <b>Clean Code</b> with <b>Test Driven Development</b> practice using testing frameworks like Jest, React Testing Library, Cucumber, Rspec, and Capybara",
+      "Involved in building highly scalable application and maintaining legacy code",
+      "Working with <b>Git</b> in an <b>Agile</b> environment",
+    ],
+  },
+];
+
+export default jobs;
